a445dad4f60562951b88c5837cf668e4f1b5d08a
JenBanks8585 <jennifer.banks8585@gmail.com>
2020-10-26 17:13:40 -0400

    setup.py

--- lambdata20/hello.py
+++ lambdata20/hello.py
@@ -4,7 +4,10 @@ print("hello")
 # or from lambdata20.mymod import enlarge
 
 
-
+from pandas import DataFrame
 from mymod import enlarge
 
 print(enlarge(2))
+
+df = DataFrame({'A' :[2, 4] })
+print(df.head())
---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="lambdata_DS20", # the name that you will install via pip
+    version="1.0",
+    author="JBanks",
+    author_email="jennifer.banks8585@gmail.com",
+    description="A short description",
+    long_description=long_description,
+    long_description_content_type="text/markdown", # required if using a md file for long desc
+    #license="MIT",
+    url="https://github.com/JenBanks8585/Lambdata_ds20",
+    #keywords="",
+    packages=find_packages() # ["my_lambdata"]
+)
