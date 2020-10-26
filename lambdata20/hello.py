c11ff693e2c91687edf49348a486d6245cb54595
JenBanks8585 <jennifer.banks8585@gmail.com>
2020-10-26 16:46:19 -0400

    update

---
+++ lambdata20/hello.py
@@ -0,0 +1,10 @@
+print("hello")
+
+# from module import function
+# or from lambdata20.mymod import enlarge
+
+
+
+from mymod import enlarge
+
+print(enlarge(2))
---
+++ lambdata20/mymod.py
@@ -0,0 +1,16 @@
+
+def enlarge(n):
+    """
+    Param n is a number
+    Function will enlarge the number
+    """
+    return n * 100
+
+#y = int(input("enter a value"))
+#print(y, enlarge(y))
+
+if __name__== "__main__":
+    print("hello")
+    
+    y = int(input("enter a value"))
+    print(y, enlarge(y))
